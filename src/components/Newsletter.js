3262d28f5f65ffb77764bd2d7bbac7d0f823cfb6
Alphonse Kaman <kamanalphonse@gmail.com>
2023-05-12 16:35:05 +0000

    Newsletter component design over

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -18,12 +18,12 @@ export default function Newsletter() {
             <h6>Recevez votre newsletter directement dans votre boîte mail</h6>
             <label for="news" className='d-inline-block'>votre adresse email:</label>
             <div className='d-flex'>
-              <input className='col-xs-12 col-sm-8 col-md-6 col-lg-6' id='news' type='email' placeholder='Votre adresse email' onChange={handleClick}/>
+              <input className='email-input col-xs-12 col-sm-8 col-md-6 col-lg-6' id='news' type='email' placeholder='Votre adresse email' onChange={handleClick}/>
               <button><FontAwesomeIcon icon={faPaperPlane} style={{color: "#d90429",}} /></button>
             </div>
-            <div className='d-flex' >
-                <input type="checkbox" name="cheese" id="cheese" style={{marginRight: 10}}></input>
-                <label for="cheese">Votre adresse e-mail est uniquement utilisée pour vous envoyer notre newsletter et des informations sur les activités de Vinmartech. Vous pouvez toujours utiliser le lien de désinscription inclus dans la newsletter.</label>
+            <div className='checkbox d-flex' >
+                <input type="checkbox" name="cheese" id="check"></input>
+                <label for="check">Votre adresse e-mail est uniquement utilisée pour vous envoyer notre newsletter et des informations sur les activités de Vinmartech. Vous pouvez toujours utiliser le lien de désinscription inclus dans la newsletter.</label>
             </div>
           </div>
         </div>
