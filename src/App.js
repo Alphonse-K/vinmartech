9a767b36168f154827632b39e1f2e122b13a34f8
Alphonse Kaman <kamanalphonse@gmail.com>
2023-05-14 11:11:02 +0000

    Spinner component added to the project and some updates where made to app.css & app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,27 +9,38 @@ import Contact from './components/Contact';
 import NoPage from './components/NoPage';
 import Home from './components/Home';
 import Expertises from './components/Expertises';
+import Spinner from './components/Spinner';
+
 
 
 function App() {
-
+  
   const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 5000);
   }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="expertises" element={<Expertises />} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+    {
+      loading 
+      ? <Spinner />
+      :
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="expertises" element={<Expertises />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
+    }
+    </>
   );
 }
 
---
+++ src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Spinner() {
+  return (
+    <>
+        <div class="loader-container">
+            <span class="loader"></span>  
+        </div>
+    </>
+  )
+}
