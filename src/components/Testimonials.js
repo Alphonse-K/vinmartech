ddfb057d53982b56edb4a040fc91e086cf770f41
Alphonse Kaman <kamanalphonse@gmail.com>
2023-05-16 12:56:11 +0000

    Testimonials.js component updated

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React from "react";
+import SwiperCore, {Autoplay, EffectCoverflow, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
 
-export default function Testimonials() {
+SwiperCore.use([Autoplay, EffectCoverflow, Pagination]);
+
+const slide_img = [
+  "https://swiperjs.com/demos/images/nature-1.jpg",
+  "https://swiperjs.com/demos/images/nature-2.jpg",
+  "https://swiperjs.com/demos/images/nature-3.jpg",
+  "https://swiperjs.com/demos/images/nature-4.jpg",
+  "https://swiperjs.com/demos/images/nature-5.jpg",
+  "https://swiperjs.com/demos/images/nature-6.jpg",
+  "https://swiperjs.com/demos/images/nature-7.jpg",
+  "https://swiperjs.com/demos/images/nature-8.jpg",
+  "https://swiperjs.com/demos/images/nature-9.jpg",
+];
+
+const Testimonials = () => {
   return (
-    <div>Expertises</div>
- )
-}
+    <div>
+      <Swiper
+        autoplay={{"delay": 4000}}
+        effect={"flip"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={2}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 50,
+          modifier: 6,
+          slideShadows: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        className="mySwiper"
+      >
+        {slide_img.map((img, i) => {
+          return (
+            <SwiperSlide key={i}>
+              <img src={img} alt="" />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Testimonials;
