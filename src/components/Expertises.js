c84c3e4803e6873017318cecb57d4342b222ffdb
Alphonse Kaman <kamanalphonse@gmail.com>
2023-05-17 17:16:10 +0000

    expertise update

--- src/components/Expertises.js
+++ src/components/Expertises.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import BackToTop from './BackToTop';
+import { Bounce } from 'react-awesome-reveal';
 
 export default function Expertises() {
   return (
@@ -9,12 +10,17 @@ export default function Expertises() {
     <div>
         <Navbar/>
         <div className='expertise-header'>
-          <h1>Nous sommes spécialisés dans la conception de programmes informatiques divers, et bien d'autres.</h1>
-        <div className="arrow">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>  
+          <img src={require('../img/desktop.jpg')} alt='' />
+          <section className='expertise-text'>
+            <Bounce>
+              <h1>Nous sommes spécialisés dans la conception de programmes informatiques divers, et bien d'autres.</h1>
+            </Bounce>
+            <div className="arrow">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>  
+          </section>
         </div>
         <div>Expertises</div>
         <div>Expertises</div>
