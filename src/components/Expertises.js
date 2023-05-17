8509c079cd8d93d4cec8a6d1f4f2af8ea838285b
Alphonse Kaman <kamanalphonse@gmail.com>
2023-05-17 16:24:59 +0000

    adding  animated arrows the expertises component

--- src/components/Expertises.js
+++ src/components/Expertises.js
@@ -10,6 +10,11 @@ export default function Expertises() {
         <Navbar/>
         <div className='expertise-header'>
           <h1>Nous sommes spécialisés dans la conception de programmes informatiques divers, et bien d'autres.</h1>
+        <div className="arrow">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>  
         </div>
         <div>Expertises</div>
         <div>Expertises</div>
